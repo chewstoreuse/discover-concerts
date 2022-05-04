16e47e3f162b5fadb05096ed553cc52d7e0507f5
James <qjameso@berkeley.edu>
2022-05-03 21:37:45 -0700

    feat: complete my concerts page styles

--- client/src/components/myConcerts/MyConcerts.jsx
+++ client/src/components/myConcerts/MyConcerts.jsx
@@ -4,20 +4,38 @@ import axios from 'axios';
 const MyConcerts = () => {
   const [myConcerts, setMyConcerts] = useState([]);
 
-  // useState(() => {
-  //   axios.get('/myConcert')
-  //     .then(concerts => {
-  //       //concerts.data
-  //       setMyConcerts(concerts);
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // }, []);
+  // const handleGoing = () => {
+
+  // }
+
+  useState(() => {
+    axios.get('/myConcert')
+      .then(concerts => {
+        console.log(concerts.data)
+        setMyConcerts(concerts.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className='my-concerts-page'>
-      hello
+      <ul>
+        {myConcerts.map((concert, i) => {
+          return (
+            <li key={i}>
+              <div>
+                <h3>{concert.event}</h3>
+                {concert.location} - {concert.date} <a href={concert.url}>TICKETS</a>
+              </div>
+              <div>
+                <button type='button' className='btn btn-secondary'>Not Going</button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
