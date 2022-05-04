11a7690eb29ee2ea792c0b980451103414ecb302
James <qjameso@berkeley.edu>
2022-05-03 18:56:09 -0700

    feat: complete post route to db for adding concerts

--- server/controllers/concert.js
+++ server/controllers/concert.js
@@ -1,8 +1,31 @@
 const Concert = require('../models/concert');
 
 module.exports = {
-  getConcert: function (req, res) {
+  getMyConcert: function (req, res) {
     console.log('get request recieved');
     res.sendStatus(200);
   },
+  addMyConcert: function (req, res) {
+    console.log(req.body);
+    let conditions = {
+      event: req.body.event,
+      location: req.body.location
+    };
+
+    let update = {
+      date: req.body.date,
+      url: req.body.url
+    };
+
+    Concert.findOneAndUpdate(conditions, update, {
+      new: true,
+      upsert: true
+    })
+      .then(response => {
+        res.sendStatus(201);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 }
